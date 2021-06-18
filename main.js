0f7f5169c48858afc24c8bad55254b985ac6905e
dding-woo <70466220+dding-woo@users.noreply.github.com>
2021-06-19 01:20:23 +0900

    Update main.js
    SlideShow item List js

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
 'use strict';
 
-const bottmMenuList = document.querySelectorAll(".bottom__menu__list a");
+const slider = document.querySelector(".slider");
 
-bottmMenuList.forEach((list) => {
-    list.addEventListener("mouseover", () => {
-        list.classList.add("underline");
-    });
+const slidList = document.querySelectorAll(".slide__list");
+
+const nextBtn = document.querySelector(".next__btn");
+
+const prev__btn = document.querySelector(".prev__btn");
+let currentindex = 0;
+
+let slideCount = slidList.length
+
+// Slide item List
+nextBtn.addEventListener("click", () => {
+    if (currentindex < slideCount - 5) {
+        slidemove(currentindex + 1);
+        console.log(currentindex);
+    }
+    else {
+        slidemove(0);
+    }
 });
+
+prev__btn.addEventListener("click", () => {
+    if (currentindex > 0) {
+        slidemove(currentindex - 1);
+    }
+    else {
+        slidemove(currentindex + 5);
+    }
+
+
+});
+
+function slidemove(num) {
+    slider.style.left = -num * 216 + "px";
+    currentindex = num;
+}
